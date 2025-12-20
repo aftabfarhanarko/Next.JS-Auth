ded186cdf678ff9dc662e2cbaef6f6f062ff0cf7
Aftab Farhan <aftabfarhan324@gmail.com>
2025-12-20 16:26:12 +0600

    nextjs auth implemennt github login system

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import { dbConnect } from "@/lib/mongoDb";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
@@ -72,6 +72,10 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 
